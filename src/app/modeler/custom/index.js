d283d770d81e2ceb3830fd0a73b9ab44f6f32f06
Prakashshamala <73683449+Prakashshamala@users.noreply.github.com>
2022-05-30 22:11:07 +0530

    Update index.js

--- src/app/modeler/custom/index.js
+++ src/app/modeler/custom/index.js
@@ -1,12 +1,14 @@
-// import CustomPalette from './CustomPalette';
+import CustomPalette from './CustomUserTask';
 import CustomRenderer from './CustomRenderer';
 import CustomPaletteProvider from './CustomPaletteProvider';
 import CustomContextPadProvider from './CustomContextPadProvider';
+import MyReplaceMenuProvider from "./MyReplaceMenuProvider";
 
 export default {
-  __init__: ['customRenderer','paletteProvider', 'contextPadProvider'],
-  // customPalette: [ 'type', CustomPalette ],
+  __init__: ['customPalette','customRenderer','paletteProvider', 'contextPadProvider','myReplaceMenuProvider'],
+  customPalette: [ 'type', CustomPalette ],
   customRenderer: [ 'type', CustomRenderer ],
-  paletteProvider: ["type", CustomPaletteProvider],
+  paletteProvider: ['type', CustomPaletteProvider],
+  myReplaceMenuProvider: ['type', MyReplaceMenuProvider],
   contextPadProvider: [ 'type', CustomContextPadProvider ]
 };
